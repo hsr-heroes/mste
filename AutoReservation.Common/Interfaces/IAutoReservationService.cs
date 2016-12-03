4f507c1692c424f59b9caadca2d7ebd42f82015b
Tobias Staehli <tobiasstaehli@msn.com>
2016-12-03 17:38:07 +0100

    first version of Service implementation

--- AutoReservation.Common/Interfaces/IAutoReservationService.cs
+++ AutoReservation.Common/Interfaces/IAutoReservationService.cs
@@ -1,21 +1,34 @@
 ﻿using System.Collections.Generic;
+using System.ServiceModel;
 using AutoReservation.Common.DataTransferObjects;
 
 namespace AutoReservation.Common.Interfaces
 {
+    [ServiceContract]
     public interface IAutoReservationService
     {
+        IEnumerable<KundeDto> Kunden { [OperationContract]get; }
+        IEnumerable<AutoDto> Autos { [OperationContract] get; }
+        IEnumerable<ReservationDto> Reservationen { [OperationContract]get; }
+        [OperationContract]
         ReservationDto InsertReservation(ReservationDto reservation);
+        [OperationContract]
         ReservationDto UpdateReservation(ReservationDto reservation);
-        IEnumerable<KundeDto> Kunden { get; }
-        IEnumerable<AutoDto> Autos { get; }
-        IEnumerable<ReservationDto> Reservationen { get; }
+
+
+        [OperationContract]
         AutoDto InsertAuto(AutoDto auto);
+        [OperationContract]
         AutoDto UpdateAuto(AutoDto auto);
+        [OperationContract]
         void DeleteAuto(AutoDto selectedAuto);
+        [OperationContract]
         KundeDto InsertKunde(KundeDto kunde);
+        [OperationContract]
         KundeDto UpdateKunde(KundeDto kunde);
+        [OperationContract]
         void DeleteKunde(KundeDto selectedKunde);
+        [OperationContract]
         void DeleteReservation(ReservationDto selectedReservation);
     }
 }
--- AutoReservation.Service.Wcf/AutoReservationService.cs
+++ AutoReservation.Service.Wcf/AutoReservationService.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Diagnostics;
+using System.Linq;
 using AutoReservation.Common.DataTransferObjects;
 using AutoReservation.Common.Interfaces;
+using AutoReservation.Dal;
 
 namespace AutoReservation.Service.Wcf
 {
@@ -14,52 +18,140 @@ namespace AutoReservation.Service.Wcf
             Console.WriteLine($"Calling: {new StackTrace().GetFrame(1).GetMethod().Name}");
         }
 
+        public IEnumerable<KundeDto> Kunden
+        {
+            get
+            {
+                using (var ctx = new AutoReservationContext())
+                {
+                    WriteActualMethod();
+                    return ctx.Kunden.ToList().Select(k => k.ConvertToDto());
+                }
+            }
+        }
+
+        public IEnumerable<AutoDto> Autos { get
+            {
+                using (var ctx = new AutoReservationContext())
+                {
+                    WriteActualMethod();
+                    return ctx.Autos.ToList().Select(k => k.ConvertToDto());
+                }
+            }
+        }
+        public IEnumerable<ReservationDto> Reservationen { get
+            {
+                using (var ctx = new AutoReservationContext())
+                {
+                    WriteActualMethod();
+                    return ctx.Reservationen.Include(r => r.Auto).Include(r => r.Kunde).ToList().Select(k => k.ConvertToDto());
+                }
+            }
+        }
+
         public ReservationDto InsertReservation(ReservationDto reservation)
         {
-            throw new NotImplementedException();
+            WriteActualMethod();
+            using (var ctx = new AutoReservationContext())
+            {
+                var entity = reservation.ConvertToEntity();
+                ctx.Reservationen.AddOrUpdate(entity);
+                ctx.SaveChanges();
+                return entity.ConvertToDto();
+            }
         }
 
         public ReservationDto UpdateReservation(ReservationDto reservation)
         {
-            throw new NotImplementedException();
+            WriteActualMethod();
+            using (var ctx = new AutoReservationContext())
+            {
+                var entity = reservation.ConvertToEntity();
+                ctx.Reservationen.Attach(entity);
+                ctx.Entry(entity).State = EntityState.Modified;
+                ctx.SaveChanges();
+                return entity.ConvertToDto();
+            }
         }
 
-        public IEnumerable<KundeDto> Kunden { get; }
-        public IEnumerable<AutoDto> Autos { get; }
-        public IEnumerable<ReservationDto> Reservationen { get; }
         public AutoDto InsertAuto(AutoDto auto)
         {
-            throw new NotImplementedException();
+            WriteActualMethod();
+            using (var ctx = new AutoReservationContext())
+            {
+                var dbAuto = auto.ConvertToEntity();
+                ctx.Autos.AddOrUpdate(dbAuto);
+                ctx.SaveChanges();
+                return dbAuto.ConvertToDto();
+            }
         }
 
         public AutoDto UpdateAuto(AutoDto auto)
         {
-            throw new NotImplementedException();
+            WriteActualMethod();
+            using (var ctx = new AutoReservationContext())
+            {
+                var entity = auto.ConvertToEntity();
+                ctx.Autos.Attach(entity);
+                ctx.Entry(entity).State = EntityState.Modified;
+                ctx.SaveChanges();
+                return entity.ConvertToDto();
+            }
         }
 
         public void DeleteAuto(AutoDto selectedAuto)
         {
-            throw new NotImplementedException();
+            WriteActualMethod();
+            using (var ctx = new AutoReservationContext())
+            {
+                ctx.Autos.Remove(ctx.Autos.FirstOrDefault(e => e.Id == selectedAuto.Id));
+                ctx.SaveChanges();
+            }
         }
 
         public KundeDto InsertKunde(KundeDto kunde)
         {
-            throw new NotImplementedException();
+            WriteActualMethod();
+            using (var ctx = new AutoReservationContext())
+            {
+                var entity = kunde.ConvertToEntity();
+                ctx.Kunden.Add(entity);
+                ctx.SaveChanges();
+                return entity.ConvertToDto();
+            }
         }
 
         public KundeDto UpdateKunde(KundeDto kunde)
         {
-            throw new NotImplementedException();
+            WriteActualMethod();
+            using (var ctx = new AutoReservationContext())
+            {
+                var entity = kunde.ConvertToEntity();
+                ctx.Kunden.Attach(entity);
+                ctx.Entry(entity).State = EntityState.Modified;
+                ctx.SaveChanges();
+                return entity.ConvertToDto();
+            }
         }
 
         public void DeleteKunde(KundeDto selectedKunde)
         {
-            throw new NotImplementedException();
+            WriteActualMethod();
+            using (var ctx = new AutoReservationContext())
+            {
+                ctx.Kunden.Remove(ctx.Kunden.FirstOrDefault(e => e.Id == selectedKunde.Id));
+                ctx.SaveChanges();
+            }
         }
 
         public void DeleteReservation(ReservationDto selectedReservation)
         {
-            throw new NotImplementedException();
+            WriteActualMethod();
+            using (var ctx = new AutoReservationContext())
+            {
+                ctx.Reservationen.Remove(ctx.Reservationen.FirstOrDefault(e => e.ReservationsNr == selectedReservation.ReservationsNr));
+                ctx.SaveChanges();
+            }
         }
     }
 }
